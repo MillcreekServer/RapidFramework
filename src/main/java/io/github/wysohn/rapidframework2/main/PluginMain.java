42e80a23f1ff32cfd05ecde10db91817a450266c
wysohn <wysohn2002@naver.com>
2019-09-18 23:48:35 -0700

    upload

--- src/main/java/io/github/wysohn/rapidframework2/main/PluginMain.java
+++ src/main/java/io/github/wysohn/rapidframework2/main/PluginMain.java
@@ -1,5 +1,26 @@
 package io.github.wysohn.rapidframework2.main;
 
-public class PluginMain {
+import io.github.wysohn.rapidframework2.manager.Manager;
+import io.github.wysohn.rapidframework2.manager.command.ManagerCommand;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class PluginMain {
+    private final ManagerCommand managerCommand;
+
+    private final Map<Class<? extends Manager>, Manager> managers = new HashMap<>();
+    private final Map<String, Manager> managersStr = new HashMap<>();
+
+    private final String adminPermission;
+
+    public PluginMain(String mainCommand, String adminPermission) {
+        this.managerCommand = new ManagerCommand(mainCommand, adminPermission);
+        this.adminPermission = adminPermission;
+    }
+
+    public void registerManager(Manager manager) {
+        managers.put(manager.getClass(), manager);
+        managersStr.put(manager.getClass().getSimpleName(), manager);
+    }
 }
--- src/main/java/io/github/wysohn/rapidframework2/manager/Manager.java
+++ src/main/java/io/github/wysohn/rapidframework2/manager/Manager.java
@@ -1,4 +1,23 @@
 package io.github.wysohn.rapidframework2.manager;
 
-public class Manager {
+import io.github.wysohn.rapidframework2.main.PluginMain;
+
+public abstract class Manager {
+    public static final int FASTEST_PRIORITY = 0;
+    public static final int NORM_PRIORITY = 5;
+    public static final int SLOWEST_PRIORITY = 10;
+
+    private final PluginMain main;
+    private final int loadPriority;
+
+    public Manager(PluginMain main, int loadPriority) {
+        this.main = main;
+        this.loadPriority = loadPriority;
+    }
+
+    public abstract void enable() throws Exception;
+
+    public abstract void load() throws Exception;
+
+    public abstract void disable() throws Exception;
 }
