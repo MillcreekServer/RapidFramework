b3876b9f5347c4825a0f58ca6e7a438f5f7ad8a8
wysohn <wysohn2002@naver.com>
2020-05-10 01:42:03 -0700

    forgot to add getter

--- src/main/java/io/github/wysohn/rapidframework2/core/main/PluginMain.java
+++ src/main/java/io/github/wysohn/rapidframework2/core/main/PluginMain.java
@@ -96,6 +96,10 @@ public class PluginMain implements PluginRuntime {
         return lang;
     }
 
+    public ITaskSupervisor task() {
+        return taskSupervisor;
+    }
+
     public String getRootPermission() {
         return rootPermission;
     }
