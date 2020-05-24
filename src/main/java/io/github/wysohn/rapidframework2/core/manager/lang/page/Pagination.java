8072c1b1065ab5b5a81d0e4c51848b2972929ea9
wysohn <wysohn2002@naver.com>
2020-05-24 14:00:48 -0700

    locking mechanism support for Pagination

--- src/main/java/io/github/wysohn/rapidframework2/core/manager/lang/page/Pagination.java
+++ src/main/java/io/github/wysohn/rapidframework2/core/manager/lang/page/Pagination.java
@@ -6,21 +6,26 @@ import io.github.wysohn.rapidframework2.core.manager.common.message.Message;
 import io.github.wysohn.rapidframework2.core.manager.common.message.MessageBuilder;
 import io.github.wysohn.rapidframework2.core.manager.lang.DefaultLangs;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Pagination<T> {
+    private final ExecutorService exec = Executors.newCachedThreadPool();
+
     public static String LEFT_ARROW = "&8[&a<---&8]";
     public static String HOME = "&8[&aHome&8]";
     public static String RIGHT_ARROW = "&8[&a--->&8]";
 
     private final PluginMain main;
-    private final DataProvider<T> list;
+    private final DataProvider<T> dataProvider;
     private final int max;
     private final String title;
 
     private final String cmd;
 
-    public Pagination(PluginMain main, DataProvider<T> list, int max, String title, String cmd) {
+    public Pagination(PluginMain main, DataProvider<T> dataProvider, int max, String title, String cmd) {
         this.main = main;
-        this.list = list;
+        this.dataProvider = dataProvider;
         this.max = max;
         this.title = title;
         this.cmd = cmd;
@@ -37,62 +42,68 @@ public class Pagination<T> {
                 langman.addString(title)));
         main.lang().sendRawMessage(sender, MessageBuilder.forMessage("").build());
 
-        int remainder = list.size() % max;
-        int divided = list.size() / max;
-        int outof = remainder == 0 ? divided : divided + 1;
+        exec.submit(() -> dataProvider.sync(() -> {
+            int remainder = dataProvider.size() % max;
+            int divided = dataProvider.size() / max;
+            int outof = remainder == 0 ? divided : divided + 1;
 
-        page = Math.max(page, 0);
-        page = Math.min(page, outof - 1);
+            int p = Math.max(page, 0);
+            p = Math.min(p, outof - 1);
 
-        int index;
-        for (index = page * max; index >= 0 && index < (page + 1) * max; index++) {
-            if (index >= list.size())
-                break;
+            int index;
+            for (index = p * max; index >= 0 && index < (p + 1) * max; index++) {
+                if (index >= dataProvider.size())
+                    break;
 
-            T val = list.get(index);
-            if (list.omit(val)) {
-                continue;
+                T val = dataProvider.get(index);
+                if (dataProvider.omit(val)) {
+                    continue;
+                }
+
+                main.lang().sendRawMessage(sender, messageFn.convert(sender, val, index));
             }
 
-            main.lang().sendRawMessage(sender, messageFn.convert(sender, val, index));
-        }
+            main.lang().sendMessage(sender, DefaultLangs.General_Line);
 
-        main.lang().sendMessage(sender, DefaultLangs.General_Line);
-
-        if (main.lang().isJsonEnabled()) {
-            final String cmdPrev = this.cmd + " " + page;
-            final String cmdHome = this.cmd;
-            final String cmdNext = this.cmd + " " + (page + 2);
+            if (main.lang().isJsonEnabled()) {
+                final String cmdPrev = this.cmd + " " + p;
+                final String cmdHome = this.cmd;
+                final String cmdNext = this.cmd + " " + (p + 2);
 
 
-            Message[] btns = MessageBuilder.forMessage("")
-                    .append(LEFT_ARROW)
-                    .withHoverShowText(cmdPrev)
-                    .withClickRunCommand(cmdPrev)
-                    .append(HOME)
-                    .withHoverShowText(cmdHome)
-                    .withClickRunCommand(cmdHome)
-                    .append(RIGHT_ARROW)
-                    .withHoverShowText(cmdNext)
-                    .withClickRunCommand(cmdNext)
-                    .build();
+                Message[] btns = MessageBuilder.forMessage("")
+                        .append(LEFT_ARROW)
+                        .withHoverShowText(cmdPrev)
+                        .withClickRunCommand(cmdPrev)
+                        .append(HOME)
+                        .withHoverShowText(cmdHome)
+                        .withClickRunCommand(cmdHome)
+                        .append(RIGHT_ARROW)
+                        .withHoverShowText(cmdNext)
+                        .withClickRunCommand(cmdNext)
+                        .build();
 
-            main.lang().sendRawMessage(sender, btns);
-        } else {
-            main.lang().sendMessage(sender, DefaultLangs.Command_Help_TypeHelpToSeeMore, ((sen, langman) ->
-                    langman.addString(this.cmd + " <page>")));
-        }
+                main.lang().sendRawMessage(sender, btns);
+            } else {
+                main.lang().sendMessage(sender, DefaultLangs.Command_Help_TypeHelpToSeeMore, ((sen, langman) ->
+                        langman.addString(this.cmd + " <page>")));
+            }
 
-        final int pageCopy = page + 1;
-        main.lang().sendMessage(sender, DefaultLangs.Command_Help_PageDescription, ((sen, langman) ->
-                langman.addInteger(pageCopy).addInteger(outof)));
-        sender.sendMessageRaw("");
+            final int pageCopy = p + 1;
+            main.lang().sendMessage(sender, DefaultLangs.Command_Help_PageDescription, ((sen, langman) ->
+                    langman.addInteger(pageCopy).addInteger(outof)));
+            sender.sendMessageRaw("");
+        }));
     }
 
     public interface DataProvider<T> {
         int size();
 
         T get(int index);
+
+        default void sync(Runnable run) {
+            run.run();
+        }
 
         default boolean omit(T val) {
             return false;
