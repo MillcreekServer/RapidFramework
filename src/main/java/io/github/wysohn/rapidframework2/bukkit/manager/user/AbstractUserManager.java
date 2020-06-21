b88a1eed0d2c816f83414749f946a201a0a90fe0
wysohn <wysohn2002@naver.com>
2020-06-20 20:59:34 -0700

    pull methods

---
+++ src/main/java/io/github/wysohn/rapidframework2/bukkit/manager/user/AbstractUserManager.java
@@ -0,0 +1,42 @@
+package io.github.wysohn.rapidframework2.bukkit.manager.user;
+
+import io.github.wysohn.rapidframework2.bukkit.main.objects.BukkitPlayer;
+import io.github.wysohn.rapidframework2.core.manager.caching.AbstractManagerElementCaching;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.lang.ref.Reference;
+import java.util.Optional;
+import java.util.UUID;
+
+public abstract class AbstractUserManager<V extends BukkitPlayer>
+        extends AbstractManagerElementCaching<UUID, V>
+        implements Listener {
+    public AbstractUserManager(int loadPriority) {
+        super(loadPriority);
+    }
+
+    @EventHandler
+    public void onLogin(AsyncPlayerPreLoginEvent event) {
+        if (!this.get(event.getUniqueId()).map(Reference::get).isPresent()) {
+            getOrNew(event.getUniqueId());
+        }
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Optional.of(event.getPlayer())
+                .ifPresent(player -> getOrNew(player.getUniqueId())
+                        .map(Reference::get)
+                        .orElse(null)
+                        .setSender(player));
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+
+    }
+}
