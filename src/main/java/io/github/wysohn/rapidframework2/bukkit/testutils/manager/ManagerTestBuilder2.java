28c7dd60d54ec7d4f25779c0756209378e61a63f
wysohn <wysohn2002@naver.com>
2020-09-05 16:58:34 -0700

    new test builder

---
+++ src/main/java/io/github/wysohn/rapidframework2/bukkit/testutils/manager/ManagerTestBuilder2.java
@@ -0,0 +1,82 @@
+package io.github.wysohn.rapidframework2.bukkit.testutils.manager;
+
+import io.github.wysohn.rapidframework2.core.main.PluginMain;
+import io.github.wysohn.rapidframework2.core.manager.config.ManagerConfig;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+import org.mockito.internal.util.reflection.Whitebox;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ManagerTestBuilder2<M extends PluginMain.Manager> {
+    private final PluginMain main = mock(PluginMain.class);
+    private final ManagerConfig config = mock(ManagerConfig.class);
+
+    private final M manager;
+
+    private final List<Execution<M, ?>> executions = new LinkedList<>();
+
+    private ManagerTestBuilder2(M manager) {
+        this.manager = manager;
+        Whitebox.setInternalState(manager, "main", main);
+        when(main.conf()).thenReturn(config);
+    }
+
+    public static <M extends PluginMain.Manager> ManagerTestBuilder2<M> of(M manager) {
+        return new ManagerTestBuilder2<>(manager);
+    }
+
+    public ManagerTestBuilder2<M> mockAny(Execution<M, Void> fn) {
+        this.executions.add(fn);
+        return this;
+    }
+
+    public ManagerTestBuilder2<M> mockEvent(Execution<M, Event> fn) {
+        this.executions.add(fn);
+        return this;
+    }
+
+    public ManagerTestBuilder2<M> expect(Execution<M, Boolean> fn) {
+        this.executions.add(fn);
+        return this;
+    }
+
+    public boolean test() {
+        for (Execution<M, ?> execution : executions) {
+            Object result = execution.apply(manager);
+            if (result instanceof Event) {
+                Class<? extends Event> eventClass = (Class<? extends Event>) result.getClass();
+                for (Method method : manager.getClass().getMethods()) {
+                    Annotation annotation = method.getAnnotation(EventHandler.class);
+                    if (annotation == null
+                            || method.getParameterCount() != 1
+                            || method.getParameterTypes()[0] != eventClass)
+                        continue;
+
+                    try {
+                        method.invoke(manager, result);
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else if (result instanceof Boolean) {
+                if (!(boolean) result)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    @FunctionalInterface
+    public interface Execution<M extends PluginMain.Manager, T> {
+        T apply(M manager);
+    }
+}
