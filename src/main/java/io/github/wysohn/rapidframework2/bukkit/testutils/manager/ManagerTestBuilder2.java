b31db6ec66ac0aad6f237f18e1bcde3b0be67315
wysohn <wysohn2002@naver.com>
2020-09-05 23:33:13 -0700

    Change design

--- src/main/java/io/github/wysohn/rapidframework2/bukkit/testutils/manager/ManagerTestBuilder2.java
+++ src/main/java/io/github/wysohn/rapidframework2/bukkit/testutils/manager/ManagerTestBuilder2.java
@@ -5,6 +5,7 @@ import io.github.wysohn.rapidframework2.core.manager.config.ManagerConfig;
 import io.github.wysohn.rapidframework2.tools.FileUtil;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
+import org.mockito.Mockito;
 import org.mockito.internal.util.reflection.Whitebox;
 
 import java.io.File;
@@ -14,17 +15,17 @@ import java.lang.reflect.Method;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.logging.Logger;
 
 import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ManagerTestBuilder2<M extends PluginMain.Manager> {
     public static final File TEMP_FOLDER = new File("build/tmp/managertest/");
-    private final PluginMain main = mock(PluginMain.class);
-    private final Logger logger = mock(Logger.class);
-    private final ManagerConfig config = mock(ManagerConfig.class);
+    private final PluginMain main = Mockito.mock(PluginMain.class);
+    private final Logger logger = Mockito.mock(Logger.class);
+    private final ManagerConfig config = Mockito.mock(ManagerConfig.class);
 
     private final M manager;
 
@@ -50,7 +51,7 @@ public class ManagerTestBuilder2<M extends PluginMain.Manager> {
     }
 
     public ManagerTestBuilder2<M> enable() {
-        mockAny(manager -> {
+        mock(manager -> {
             try {
                 manager.enable();
             } catch (Exception e) {
@@ -63,7 +64,7 @@ public class ManagerTestBuilder2<M extends PluginMain.Manager> {
     }
 
     public ManagerTestBuilder2<M> disable() {
-        mockAny(manager -> {
+        mock(manager -> {
             try {
                 manager.disable();
             } catch (Exception e) {
@@ -75,48 +76,62 @@ public class ManagerTestBuilder2<M extends PluginMain.Manager> {
         return this;
     }
 
-    public ManagerTestBuilder2<M> mockAny(Execution<M, Void> fn) {
-        this.executions.add(fn);
-        return this;
-    }
-
+    /**
+     * Simulate Bukkit's Event
+     *
+     * @param fn function which returns Event. Event will be automatically passed to the appropriate
+     *           methods, which are annotated with EventHandler, just like how Bukkit API handles event.
+     * @return
+     */
     public ManagerTestBuilder2<M> mockEvent(Execution<M, Event> fn) {
         this.executions.add(fn);
         return this;
     }
 
-    public ManagerTestBuilder2<M> expect(Execution<M, Boolean> fn) {
+    /**
+     * @param fn function to be executed. Throw exception to interrupt (ideally using methods in Assert class)
+     * @return
+     */
+    public ManagerTestBuilder2<M> mock(Execution<M, Void> fn) {
         this.executions.add(fn);
         return this;
     }
 
+    public ManagerTestBuilder2<M> mock(Consumer<M> fn) {
+        this.mock((m) -> {
+            fn.accept(manager);
+            return null;
+        });
+        return this;
+    }
+
     public boolean test() {
-        for (Execution<M, ?> execution : executions) {
-            Object result = execution.apply(manager);
+        try {
+            for (Execution<M, ?> execution : executions) {
+                Object result = execution.apply(manager);
 
-            if (result instanceof Event) {
-                Class<? extends Event> eventClass = (Class<? extends Event>) result.getClass();
-                for (Method method : manager.getClass().getMethods()) {
-                    Annotation annotation = method.getAnnotation(EventHandler.class);
-                    if (annotation == null
-                            || method.getParameterCount() != 1
-                            || method.getParameterTypes()[0] != eventClass)
-                        continue;
+                if (result instanceof Event) {
+                    Class<? extends Event> eventClass = (Class<? extends Event>) result.getClass();
+                    for (Method method : manager.getClass().getMethods()) {
+                        Annotation annotation = method.getAnnotation(EventHandler.class);
+                        if (annotation == null
+                                || method.getParameterCount() != 1
+                                || method.getParameterTypes()[0] != eventClass)
+                            continue;
 
-                    try {
-                        method.invoke(manager, result);
-                    } catch (IllegalAccessException | InvocationTargetException e) {
-                        e.printStackTrace();
+                        try {
+                            method.invoke(manager, result);
+                        } catch (IllegalAccessException | InvocationTargetException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
-            } else if (result instanceof Boolean) {
-                if (!(boolean) result)
-                    return false;
             }
-        }
 
-        FileUtil.delete(TEMP_FOLDER);
-        return true;
+            return true;
+        } finally {
+            FileUtil.delete(TEMP_FOLDER);
+        }
     }
 
     @FunctionalInterface
