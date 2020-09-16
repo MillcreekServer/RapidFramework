7d6b4259d3e4f95b90c274dc196294a6cee57b8b
wysohn <wysohn2002@naver.com>
2020-09-16 00:08:35 -0700

    more modules

--- src/main/java/io/github/wysohn/rapidframework3/testmodules/MockMainModule.java
+++ src/main/java/io/github/wysohn/rapidframework3/testmodules/MockMainModule.java
@@ -2,6 +2,7 @@ package io.github.wysohn.rapidframework3.testmodules;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import io.github.wysohn.rapidframework3.core.api.ManagerExternalAPI;
 import io.github.wysohn.rapidframework3.core.main.ManagerConfig;
 import io.github.wysohn.rapidframework3.core.main.PluginMain;
 
@@ -16,14 +17,17 @@ public class MockMainModule extends AbstractModule {
     public final PluginMain mockMain;
     public final Logger mockLogger;
     public final ManagerConfig mockConfig;
+    public final ManagerExternalAPI mockApiManager;
 
     public MockMainModule() {
         this.mockMain = mock(PluginMain.class);
         this.mockLogger = mock(Logger.class);
         this.mockConfig = mock(ManagerConfig.class);
+        this.mockApiManager = mock(ManagerExternalAPI.class);
 
         when(mockMain.getLogger()).thenReturn(mockLogger);
         when(mockMain.conf()).thenReturn(mockConfig);
+        when(mockMain.api()).thenReturn(mockApiManager);
         when(mockConfig.get(eq("dbType"))).thenReturn(Optional.of("file"));
     }
 
