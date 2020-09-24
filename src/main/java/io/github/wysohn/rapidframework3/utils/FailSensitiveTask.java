4debe4871e8c4263ae0d270967c5441d10076b2f
wysohn <wysohn2002@naver.com>
2020-09-23 21:30:39 -0700

    Accept values other than boolean

--- src/main/java/io/github/wysohn/rapidframework3/utils/FailSensitiveTask.java
+++ src/main/java/io/github/wysohn/rapidframework3/utils/FailSensitiveTask.java
@@ -1,41 +1,42 @@
 package io.github.wysohn.rapidframework3.utils;
 
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
-public class FailSensitiveTask {
-    private final Supplier<Boolean> task;
+public class FailSensitiveTask<T> {
+    private final Supplier<T> task;
 
     private Runnable onFail;
     private Consumer<Exception> exceptionHandle = (ex) -> {
     };
 
-    private FailSensitiveTask(Supplier<Boolean> task) {
+    private FailSensitiveTask(Supplier<T> task) {
         this.task = task;
     }
 
-    public static FailSensitiveTask of(Supplier<Boolean> task) {
-        return new FailSensitiveTask(task);
+    public static <T> FailSensitiveTask<T> of(Supplier<T> task) {
+        return new FailSensitiveTask<T>(task);
     }
 
-    public FailSensitiveTask onFail(Runnable task) {
+    public FailSensitiveTask<T> onFail(Runnable task) {
         this.onFail = task;
         return this;
     }
 
-    public FailSensitiveTask handleException(Consumer<Exception> consumer) {
+    public FailSensitiveTask<T> handleException(Consumer<Exception> consumer) {
         this.exceptionHandle = consumer;
         return this;
     }
 
-    public boolean run() {
-        boolean result = false;
+    public T run(Predicate<T> fn) {
+        T result = null;
         try {
             result = task.get();
         } catch (Exception ex) {
             exceptionHandle.accept(ex);
         } finally {
-            if (!result)
+            if (!fn.test(result))
                 onFail.run();
         }
 
