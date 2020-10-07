2aae28f4d2caed18492f1113e066230fd9479b87
wysohn <wysohn2002@naver.com>
2020-10-06 17:59:45 -0700

    and also ignore abstract class

--- src/main/java/io/github/wysohn/rapidframework3/core/inject/module/TypeAsserterModule.java
+++ src/main/java/io/github/wysohn/rapidframework3/core/inject/module/TypeAsserterModule.java
@@ -5,13 +5,15 @@ import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import io.github.wysohn.rapidframework3.interfaces.serialize.ITypeAsserter;
 
+import java.lang.reflect.Modifier;
+
 public class TypeAsserterModule extends AbstractModule {
     @Provides
     @Singleton
     ITypeAsserter getAsserter() {
         return (type) -> {
-            //ignore interface.
-            if (type.isInterface())
+            //ignore interface or abstract class.
+            if (type.isInterface() || Modifier.isAbstract(type.getModifiers()))
                 return;
 
             try {
