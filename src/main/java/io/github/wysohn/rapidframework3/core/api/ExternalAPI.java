4eea5f232d4548041a32582508c68d93f737958a
wysohn <wysohn2002@naver.com>
2020-11-21 13:51:58 -0800

    Dependency injection support for ExternalAPI
    And some additional javadocs

--- src/main/java/io/github/wysohn/rapidframework3/core/api/ExternalAPI.java
+++ src/main/java/io/github/wysohn/rapidframework3/core/api/ExternalAPI.java
@@ -3,10 +3,35 @@ package io.github.wysohn.rapidframework3.core.api;
 import io.github.wysohn.rapidframework3.core.main.PluginMain;
 import io.github.wysohn.rapidframework3.interfaces.plugin.PluginRuntime;
 
+/**
+ * Parent class for any manager that handles external APIs.
+ *
+ * This class will be dynamically instantiated when the target
+ * plugin, which contains the API you are trying to use, exist
+ * to avoid NoClassDefFoundError. Such error can be avoided
+ * by not instantiating the class until the target plugin is found.
+ *
+ * Child class must have public constructor with PluginMain and String
+ * as arguments, and those arguments will be filled-in runtime
+ * when the target plugin is found.
+ *
+ * Plus, this class support dependency injection upon instantiation,
+ * so you may choose to annotate the constructor or field with
+ * {@link com.google.inject.Inject} or {@link javax.inject.Inject}
+ * so that dependent classes can be automatically injected runtime.
+ */
 public abstract class ExternalAPI implements PluginRuntime {
     protected final PluginMain main;
     protected final String pluginName;
 
+    /**
+     *
+     * @param main plugin main for your plugin
+     * @param pluginName name of the hooked plugin. This will
+     *                   match exactly with the 'name' of the
+     *                   plugin.yml, definition of the target
+     *                   plugin.
+     */
     public ExternalAPI(PluginMain main, String pluginName) {
         this.main = main;
         this.pluginName = pluginName;
